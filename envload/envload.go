fde1b6e244d5a4764866125ac44f45d10c62d40b
Yi ling <lingy93@gmail.com>
2021-09-23 01:13:42 +0800

    removed godotenv

--- envload/envload.go
+++ envload/envload.go
@@ -1,21 +1,19 @@
 package envload
 
 import (
-	"log"
 	"os"
 
-	"github.com/joho/godotenv"
 	_ "github.com/joho/godotenv/autoload"
 )
 
 func GoDotEnvVariable(key string) string {
 
 	// load .env file
-	err := godotenv.Load(".env")
+	// err := godotenv.Load(".env")
 
-	if err != nil {
-		log.Fatalf("Error loading .env file")
-	}
+	// if err != nil {
+	// 	log.Fatalf("Error loading .env file")
+	// }
 
 	return os.Getenv(key)
 }
